c9bee59caea719cccdd37839e06cab20a8ce8605
Ahzam Akhtar <ahzam8186@gmail.com>
2023-06-03 20:08:40 +0530

    setup

---
+++ components/Hero.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+const Hero = () => {
+  return (
+    <>
+      <div
+        style={{
+          zIndex: -1,
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <Image
+          src="/sun-tornado.png"
+          className="rounded"
+          alt="Mountains with snow"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
---
+++ components/Navbar.js
@@ -0,0 +1,64 @@
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
+//import { useBuzz } from "../Connector/buzz";
+import { CiLogin } from "react-icons/ci";
+//import { NavbarUtil } from "../utils/NavbarUtil";
+import { useRouter } from "next/router";
+import Image from 'next/image'
+const Navbar = () => {
+  const router = useRouter();
+  console.log(router.asPath);
+  //const { initialized } = useBuzz();
+  const initialized = true
+  const WalletMultiButtonDynamic = dynamic(
+    async () =>
+      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false }
+  );
+  const { word, home } = ["Login" , true]
+
+  return (
+    <>
+      <header class="text-gray-600 body-font bg-transparent absolute top-0 left-0 right-0 z-50">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+           {/* <Image src="/logo.png" alt="image" width={90} height={90} className="rounded-xl text-5xl"/> */}
+            <h1 class="ml-3 text-2xl text-black">shrine</h1>
+          </a>
+          <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+            <a class="mr-5 hover:text-white text-xl"></a>
+               
+          </nav>
+          <WalletMultiButtonDynamic
+            style={{
+              marginRight: "10px",
+              borderRadius: "50vw",
+            }}
+          />
+          <button class="bg-white text-black py-2 px-4 rounded-3xl inline-flex items-center">
+            <span>{word}</span>
+            {initialized ? (
+              <>
+                {home ? (
+                  <>
+                    <BsArrowLeft onClick={()=> router.push("/")}  className="ml-1 w-5 text-3xl" />
+                  </>
+                ) : (
+                  <>
+                    <BsArrowRight onClick={()=> router.push("/main")}  className="ml-1 w-5 text-3xl" />
+                  </>
+                )}
+              </>
+            ) : (
+              <CiLogin className="ml-1 w-5 text-3xl" />
+            )}
+          </button>
+        </div>
+      </header>
+
+    </>
+  );
+};
+
+export default Navbar;
