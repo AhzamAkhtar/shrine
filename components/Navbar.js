9f024fb88c102f351b5b0f257f1e9febb283a7ec
Ahzam Akhtar <ahzam8186@gmail.com>
2023-06-04 22:00:00 +0530

    ui done

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,26 +4,27 @@ import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
 //import { useBuzz } from "../Connector/buzz";
 import { CiLogin } from "react-icons/ci";
 //import { NavbarUtil } from "../utils/NavbarUtil";
+import styles from '../styles/Wallet.module.css'
 import { useRouter } from "next/router";
-import Image from 'next/image'
+import Image from "next/image";
 const Navbar = () => {
   const router = useRouter();
   console.log(router.asPath);
   //const { initialized } = useBuzz();
-  const initialized = true
+  const initialized = true;
   const WalletMultiButtonDynamic = dynamic(
     async () =>
       (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
     { ssr: false }
   );
-  const { word, home } = ["Login" , true]
+  const { word, home } = ["Login", true];
 
   return (
     <>
-      <header class="text-gray-600 body-font bg-transparent absolute top-0 left-0 right-0 z-50">
+      {/* <header class="text-gray-600 body-font bg-transparent absolute top-0 left-0 right-0 z-50">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-           {/* <Image src="/logo.png" alt="image" width={90} height={90} className="rounded-xl text-5xl"/> */}
+         
             <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-yellow-300 md:text-5xl lg:text-5xl dark:text-white">shrine.</h1>
           </a>
           <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-black	flex flex-wrap items-center text-base justify-center">
@@ -55,8 +56,72 @@ const Navbar = () => {
             )}
           </button>
         </div>
-      </header>
+      </header> */}
 
+      <nav class="text-gray-600 body-font bg-transparent absolute top-0 left-0 right-0 z-50">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" class="flex items-center">
+            {/* <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              class="h-8 mr-3"
+              alt="Flowbite Logo"
+            /> */}
+            <span class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-yellow-300 md:text-5xl lg:text-5xl dark:text-white">
+              shrine.
+            </span>
+          </a>
+          <div class="flex md:order-2">
+           <WalletMultiButtonDynamic className={styles.walletbutton}/>
+            <button
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 bg-transparent"
+            id="navbar-cta"
+          >
+            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0">
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:p-0 font-bold tracking-tight text-2xl"
+                  aria-current="page"
+                >
+                  home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white rounded md:hover:bg-transparent md:p-0 font-bold tracking-tight text-2xl"
+                >
+                  about
+                </a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
--- components/PaymentModal.js
+++ components/PaymentModal.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillYoutube } from "react-icons/ai";
 import Image from "next/image";
-import { Card, Dropdown } from "flowbite-react";
 const PaymentModal = () => {
   const friendLoading = false;
+  const [price, setPrice] = useState("$25");
+  const [color, setColor] = useState("green-500");
   return (
     <>
       <section class="text-gray-600 body-font ">
         <div class="container px-2 py-24 mx-auto m-auto flex sm:flex-nowrap flex-wrap">
-          {/* <div class="lg:w-2/3 md:w-1/2 bg-black rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative"></div> */}
-          <div class="lg:w-1/3 md:w-1/2 bg-black rounded-xl p-8 flex flex-col  md:ml-auto  mt-5 md:mt-0  z-10 shadow-md">
+          <div class="lg:w-1/3 md:w-1/2 bg-black rounded-xl p-8 flex flex-col  md:ml-auto  mt-2 md:mt-0  z-10 shadow-md">
             <h2 class="text-lg font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-3xl dark:text-white">
               crypto chat
             </h2>
-            
+            <p class="text-xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-lg mt-2 dark:text-white">
+              support content creators with crypto 
+            </p>
             <div className="flex flex-wrap justify-center">
               <div className="w-6/12 sm:w-4/12">
                 <img
@@ -34,21 +36,47 @@ const PaymentModal = () => {
               <h2 class="text-lg font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-3xl dark:text-white">
                 amount
               </h2>
-              <h1 className="text-lg font-extrabold leading-none tracking-tight text-green-500 md:text-5xl lg:text-5xl dark:text-white text-center">
-                $25
+              <h1
+                className={`text-lg font-extrabold leading-none tracking-tight text-${color} md:text-5xl lg:text-5xl dark:text-white text-center`}
+              >
+                {price}
               </h1>
             </div>
             <div className="flex justify-between mt-2 ">
-              <button class="bg-white text-green-500  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white ">
+              <button
+                class="bg-white text-green-500  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white "
+                onClick={() => {
+                  setPrice("$25");
+                  setColor("green-500");
+                }}
+              >
                 send $25
               </button>
-              <button class="bg-white text-blue-500  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white ">
+              <button
+                class="bg-white text-blue-500  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white "
+                onClick={() => {
+                  setPrice("$50");
+                  setColor("blue-500");
+                }}
+              >
                 send $50
               </button>
-              <button class="bg-white text-orange-400  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white ">
+              <button
+                class="bg-white text-orange-400  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white "
+                onClick={() => {
+                  setPrice("$75");
+                  setColor("orange-400 ");
+                }}
+              >
                 send $75
               </button>
-              <button class="bg-white text-red-500  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white ">
+              <button
+                class="bg-white text-red-500  py-2 px-2 rounded-lg text-lg font-extrabold leading-none tracking-tight md:text-5xl lg:text-xl dark:text-white "
+                onClick={() => {
+                  setPrice("$100");
+                  setColor("red-500");
+                }}
+              >
                 send $100
               </button>
             </div>
@@ -63,13 +91,14 @@ const PaymentModal = () => {
                 class="w-full mt-4 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-10 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               ></input>
             </div>
-            <button class="text-black bg-green-500 border-0 py-2 px-6 focus:outline-none rounded-lg text-lg  font-extrabold leading-none tracking-tight  md:text-5xl lg:text-xl dark:text-white">
-              send $100
+            <button
+              class={`text-black bg-${color} border-0 py-2 px-6 focus:outline-none rounded-lg text-lg  font-extrabold leading-none tracking-tight  md:text-5xl lg:text-xl dark:text-white`}
+            >
+              send {price}
             </button>
           </div>
           <div class="lg:w-1/2 md:w-1/2 bg-black rounded-xl overflow-hidden sm:mr-10 p-10 mx-5 flex items-end justify-start relative">
             <div
-             
               width="100%"
               height="100%"
               class="absolute inset-0 bg-black rounded-3xl"
@@ -80,12 +109,19 @@ const PaymentModal = () => {
               scrolling="no"
               src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
             >
-            <h2 className='flex justify-left mx-10 mt-10 text-white text-8xl   font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white'>do</h2>
-              <h2 className='flex justify-left mx-10 text-white text-8xl    font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white'>support</h2>
-              <h2 className='flex justify-left mx-10 text-white text-8xl    font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white'>your</h2>
-              <h2 className='flex justify-left mx-10 text-yellow-300 text-8xl mt-5 mb-4  font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white'>faviourite creator. </h2>
+              <h2 className="flex justify-left mx-10 mt-10 text-white text-8xl   font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white">
+                do
+              </h2>
+              <h2 className="flex justify-left mx-10 text-white text-8xl    font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white">
+                support
+              </h2>
+              <h2 className="flex justify-left mx-10 text-white text-8xl    font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white">
+                your
+              </h2>
+              <h2 className="flex justify-left mx-10 text-yellow-300 text-8xl mt-5 mb-4  font-extrabold leading-none tracking-tight md:text-5xl lg:text-8xl dark:text-white">
+                faviourite creator.{" "}
+              </h2>
             </div>
-           
           </div>
         </div>
       </section>
