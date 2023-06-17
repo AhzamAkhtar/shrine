813fb2c3a6028fbccaf897a7eaa9ef05ff733bbc
Ahzam Akhtar <ahzam8186@gmail.com>
2023-06-17 13:03:49 +0530

    toggle done

--- pages/cryptochat/[slug].js
+++ pages/cryptochat/[slug].js
@@ -24,9 +24,10 @@ const PaymentModal = (props) => {
   const [qrCode, setQrCode] = useState(false);
   const [message, setMessage] = useState();
   const [msgColor, setMsgColor] = useState("orange-400");
-  const [showChat, setShowChat] = useState(true);
+  const [showChat, setShowChat] = useState(false);
   const [messageData, setmessageData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [toggleHeading , setToggleHeading] = useState("Show Chat")
 
   const { publicKey, userAddress } = useCashApp();
 
@@ -39,6 +40,7 @@ const PaymentModal = (props) => {
   console.log(userPubkey);
 
   useEffect(() => {
+    setLoading(true);
     const getMessageData = async () => {
       const array = [];
       const querySnapshot = await getDocs(collection(db, "message"));
@@ -46,17 +48,20 @@ const PaymentModal = (props) => {
         array.push(doc.data());
       });
       setmessageData(array);
+      setLoading(false);
     };
     getMessageData();
     console.log(messageData);
   }, []);
 
   useEffect(() => {
+    setLoading(true);
     const getData = async () => {
       const querySnapshot = await getDocs(collection(db, "cryptochat"));
       querySnapshot.forEach((doc) => {
         if (doc.data().name == user) {
           setData(doc.data());
+          setLoading(false);
         }
       });
     };
@@ -95,6 +100,17 @@ const PaymentModal = (props) => {
     }
   };
 
+  const toggle = () => {
+    setShowChat(!showChat);
+
+    if(toggleHeading=="Show Chat"){
+      setToggleHeading("Support")
+    }
+    if(toggleHeading =="Support"){
+      setToggleHeading("Show Chat")
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -103,9 +119,23 @@ const PaymentModal = (props) => {
       {showChat ? (
         <>
           <section class="text-gray-600 body-font mx-5 py-24">
+            <div className="flex justify-center">
+              <label class="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  class="sr-only peer"
+                  checked
+                  onClick={() => toggle()}
+                />
+                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span class="ml-3 text-sm font-medium text-white dark:text-gray-300">
+                  {toggleHeading}
+                </span>
+              </label>
+            </div>
             <div class="container px-20 py-5 mx-auto ">
               <div class="flex flex-wrap -m-5 px-24 py-1 sm:px-2 ">
-                {}
                 {messageData.map((item, keys) => {
                   return (
                     <>
@@ -171,6 +201,18 @@ const PaymentModal = (props) => {
         <>
           <div className="flex justify-center absolute w-full items-center py-5 mt-10 rounded-xl">
             <div class="bg-black w-1/3 p-10 rounded-3xl">
+              <label class="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  class="sr-only peer"
+                  onClick={() => toggle()}
+                />
+                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span class="ml-3 text-sm font-medium text-white dark:text-gray-300">
+                  {toggleHeading}
+                </span>
+              </label>
               <div className="flex justify-between">
                 <h2 class="text-lg font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-3xl dark:text-white">
                   crypto chat
@@ -184,11 +226,38 @@ const PaymentModal = (props) => {
               </p>
               <div className="flex flex-wrap justify-center">
                 <div className="w-6/12 sm:w-4/12">
-                  <img
+                  {loading ? (
+                    <>
+                      <div role="status" className="flex justify-center">
+                        <svg
+                          aria-hidden="true"
+                          class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                    <img
                     src={data.profilePhoto}
                     alt="..."
                     className="shadow-lg rounded-full max-w-full h-auto align-middle border-none mt-5"
                   />
+                    </>
+                  )}
+                  
                   <div className="flex  justify-center items-center mx-auto ">
                     <h2 class="text-base font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-lg dark:text-white text-center mt-2">
                       {data.name}
