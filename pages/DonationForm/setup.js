a5c1107e750ed90397c92bd6f22aa4288a33c2b2
Ahzam Akhtar <ahzam8186@gmail.com>
2023-06-29 01:51:34 +0530

    phase1 commit

--- pages/DonationForm/setup.js
+++ pages/DonationForm/setup.js
@@ -190,7 +190,7 @@ export default function Example() {
                         <div className="flex justify-center" role="status">
                           <svg
                             aria-hidden="true"
-                            class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400"
+                            class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-300"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
