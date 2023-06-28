de434fdc465107a94944bd81927585091b917626
Ahzam Akhtar <ahzam8186@gmail.com>
2023-06-29 01:19:01 +0530

    phase1 commit

--- pages/DonationForm/setup.js
+++ pages/DonationForm/setup.js
@@ -36,6 +36,7 @@ export default function Example() {
   const [activationLink, setActivationLink] = useState();
   const [category, setCategory] = useState("");
   const [publish_afterPublish, setPublish_afterPublish] = useState("publish");
+  const [publishICon, setPublishIcon] = useState(false);
 
   useEffect(() => {
     if (connected) {
@@ -88,6 +89,7 @@ export default function Example() {
     }
     setLoading(false);
     setPublish_afterPublish("launch your donation page");
+    setPublishIcon(true);
   };
 
   const goToDonationPage = () => {
@@ -116,11 +118,11 @@ export default function Example() {
         <>
           <div className="flex justify-center py-36">
             <Card color="transparent" shadow={false}>
-              <Typography variant="h2" color="white">
+              <Typography variant="h1" color="white">
                 publish your
               </Typography>
               <Typography
-                variant="h2"
+                variant="h1"
                 className="font-semibold text-yellow-300"
               >
                 donation page
@@ -138,10 +140,33 @@ export default function Example() {
                   <div class="mb-6">
                     <label
                       for="default-input"
-                      class="block font-semibold mb-2 text-white dark:text-white"
+                      class="block text-xl font-semibold mb-2 text-white dark:text-white"
                     >
                       your page link
                     </label>
+                    <div className="flex text-xl">
+                      {publishICon ? (
+                        <>
+                          <label
+                            for="default-input"
+                            class="block font-semibold mb-4 text-white dark:text-white"
+                          >
+                            status : deployed
+                          </label>
+                          <span class="flex  mt-2.5 ml-2 w-3 h-3 bg-green-500 rounded-full"></span>
+                        </>
+                      ) : (
+                        <>
+                          <label
+                            for="default-input"
+                            class="block font-semibold mb-4 text-white dark:text-white"
+                          >
+                            status : not deployed
+                          </label>
+                          <span class="flex mt-2.5 ml-2 w-3 h-3 bg-red-500 rounded-full"></span>
+                        </>
+                      )}
+                    </div>
                     <input
                       type="text"
                       id="default-input"
@@ -154,30 +179,25 @@ export default function Example() {
                   <>
                     {loading ? (
                       <>
-                        <button
-                          disabled
-                          type="button"
-                          class="w-full text-white bg-black border border-white focus:ring-4 focus:outline-none rounded-full text-sm font-semibold px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
-                        >
+                        <div className="flex justify-center" role="status">
                           <svg
                             aria-hidden="true"
-                            role="status"
-                            class="inline w-4 h-4 mr-3 text-white animate-spin"
+                            class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
                           >
                             <path
                               d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                              fill="#E5E7EB"
+                              fill="currentColor"
                             />
                             <path
                               d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                              fill="currentColor"
+                              fill="currentFill"
                             />
                           </svg>
-                          Loading...
-                        </button>
+                          <span class="sr-only">Loading...</span>
+                        </div>
                       </>
                     ) : (
                       <>
